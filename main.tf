d59f98cd51a40d3f83ff08a195d9dec1088cfeb9
Sophia Tang <Finenyaco@users.noreply.github.com>
2023-10-26 10:52:09 +0800

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,20 @@
+variable "name" {
+  default = "testing"
+}
+
+data "alicloud_zones" "default" {
+  available_resource_creation = "VSwitch"
+}
+
+resource "alicloud_vpc" "default" {
+  name       = var.name
+  cidr_block = "172.16.0.0/12"
+}
+
+resource "alicloud_vswitch" "default" {
+  vpc_id       = alicloud_vpc.default.id
+  cidr_block   = "172.16.0.0/21"
+  zone_id      = data.alicloud_zones.default.zones[0].id
+  vswitch_name = var.name
+}
+}
